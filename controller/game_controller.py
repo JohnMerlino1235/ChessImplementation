8e90de5e34514a4b4c1c87cd7cdd40626b8d5c3c
John <john.merlino1235@gmail.com>
2022-05-08 13:51:51 -0400

    Added more methods to Controller -> get_move, update_board, game_over. TODO: play_turn

--- controller/game_controller.py
+++ controller/game_controller.py
@@ -41,3 +41,40 @@ class Controller:
         winner = self.model.get_winner()
         self.view.display_winner(winner)
 
+    def play_turn(self, piece_x, piece_y, move_x, move_y):
+
+    def get_move(self):
+        move = [0]
+        valid = False
+        while len(move) != 2 and not valid:
+            move = self.model.curr_player.receive_move()
+            if move == "exit":
+                return [(-1, -1), (-1, -1)]
+            try:
+                piece = move[0].split(',')
+                piece_x = int(piece[0])
+                piece_y = int(piece[1])
+                move = move[1].split(',')
+                move_x = int(move[0])
+                move_y = int(move[1])
+                valid = True
+            except (ValueError, IndexError) as e:
+                self.view.display_illegal_move
+                move = [0]
+                continue
+
+        return piece_x, piece_y, move_x, move_y
+
+    def update_board(self):
+        self.view.display_board()
+
+        if not self.model.is_checkmate():
+            self.view.display_curr_player(self.model.curr_player)
+        else:
+            self.game_over()
+
+    def game_over(self):
+        self.view.display_board()
+        # self.model.update_elo(self.model.get_winner())
+        self.view.display_winner(self.model.get_winner())
+

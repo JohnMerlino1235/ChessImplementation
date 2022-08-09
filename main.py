f722da5f41e67a8c9ccf90bfba2c7381059d48c5
John Merlino <johnmerlino@Johns-MacBook-Pro.local>
2022-08-09 13:51:31 -0400

    random changes

--- main.py
+++ main.py
@@ -2,22 +2,16 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from model.color import Color
 from model.game_model import Game
-from model.piece_bishop import Bishop
-from model.piece_knight import Knight
-from model.piece_pawn import Pawn
-from model.piece_rook import Rook
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     board = Game()
     print(" 0 1 2 3 4 5 6 7 ")
     for x in range(8):
--- view/gui_view.py
+++ view/gui_view.py
@@ -4,13 +4,14 @@ import tkinter as tk
 import tkinter.messagebox
 from tkinter import messagebox
 
-from PIL import ImageTk, Image
-
-from controller.game_controller import Controller
+# from controller.game_controller import Controller
 from model.color import Color
 from model.game_model import Game
 from model.player import Player
+from PIL import Image, ImageTk
+
 from view.game_view import GameView
+
 
 class GUIView(tk.Tk):
     def __init__(self, game_controller: Controller, model: Game):
@@ -49,28 +50,41 @@ class LoginPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        username_label = tk.Label(self, text='Username:')
+        username_label = tk.Label(self, text="Username:")
         username_label.grid(row=0, column=0, sticky=tk.W, padx=5, pady=5)
         username_entry = tk.Entry(self)
         username_entry.grid(row=0, column=1, sticky=tk.E, padx=5, pady=5)
-        password_label = tk.Label(self, text='Password:')
+        password_label = tk.Label(self, text="Password:")
         password_label.grid(row=1, column=0, sticky=tk.W, padx=5, pady=5)
         password_entry = tk.Entry(self)
         password_entry.grid(row=1, column=1, sticky=tk.E, padx=5, pady=5)
-        login_button = tk.Button(self, text='Login', width=10,
-                                 command=lambda: [self.login_verify(username_entry.get(), password_entry.get())])
+        login_button = tk.Button(
+            self,
+            text="Login",
+            width=10,
+            command=lambda: [
+                self.login_verify(username_entry.get(), password_entry.get())
+            ],
+        )
         login_button.grid(row=2, column=1, sticky=tk.E, padx=5, pady=5)
 
-        guest_button = tk.Button(self, text='Play as Guest', width=10,
-                                 command=lambda: self.guest_play())
+        guest_button = tk.Button(
+            self, text="Play as Guest", width=10, command=lambda: self.guest_play()
+        )
         guest_button.grid(row=2, column=0, sticky=tk.W, padx=5, pady=5)
 
-        register_button = tk.Button(self, text='Sign Up', width=20,
-                                    command=lambda: controller.change_page("RegisterPage"))
+        register_button = tk.Button(
+            self,
+            text="Sign Up",
+            width=20,
+            command=lambda: controller.change_page("RegisterPage"),
+        )
         register_button.grid(row=2, column=3, sticky=tk.W, padx=5, pady=5)
 
     def guest_play(self):
-        self.controller.game_controller.model.player_white = Player("Guest", Color.WHITE)
+        self.controller.game_controller.model.player_white = Player(
+            "Guest", Color.WHITE
+        )
         self.controller.change_page("MainPage")
 
 
@@ -81,16 +95,23 @@ class MainPage(tk.Frame):
         self.parent = parent
         label = tk.Label(self, text="Main Page")
         label.pack(side="top", pady=10)
-        play_button = tk.Button(self, text="Play Game",
-                                command=lambda: controller.change_page("PlayPage"))
-        settings_button = tk.Button(self, text="Settings",
-                                    command=lambda: controller.change_page("SettingsPage"))
-        leaderboard_button = tk.Button(self, text="View Leaderboard",
-                                       command=lambda: controller.change_page("LeaderboardPage"))
-        login_page_button = tk.Button(self, text="Sign out",
-                                      command=lambda: self.sign_out())
-        exit_button = tk.Button(self, text="Exit",
-                                command=lambda: self.close())
+        play_button = tk.Button(
+            self, text="Play Game", command=lambda: controller.change_page("PlayPage")
+        )
+        settings_button = tk.Button(
+            self,
+            text="Settings",
+            command=lambda: controller.change_page("SettingsPage"),
+        )
+        leaderboard_button = tk.Button(
+            self,
+            text="View Leaderboard",
+            command=lambda: controller.change_page("LeaderboardPage"),
+        )
+        login_page_button = tk.Button(
+            self, text="Sign out", command=lambda: self.sign_out()
+        )
+        exit_button = tk.Button(self, text="Exit", command=lambda: self.close())
         play_button.pack(pady=5)
         settings_button.pack(pady=5)
         leaderboard_button.pack(pady=5)
@@ -106,15 +127,20 @@ class PlayPage(tk.Frame, GameView):
         self.controller = controller
         self.board = board
         self.board_size = board.shape[0]
-        self.buttons = [[0 for x in range(self.board_size)] for y in range(self.board_size)]
-        exit_button = tk.Button(self, text="Exit Game",
-                                command=lambda: self.display_exit())
-        self.invalid_move_label = tk.Label(self, text="Invalid move.\nTry again.", fg="red")
+        self.buttons = [
+            [0 for x in range(self.board_size)] for y in range(self.board_size)
+        ]
+        exit_button = tk.Button(
+            self, text="Exit Game", command=lambda: self.display_exit()
+        )
+        self.invalid_move_label = tk.Label(
+            self, text="Invalid move.\nTry again.", fg="red"
+        )
         for i in range(self.board_size):
             self.rowconfigure(i, minsize=60)
             self.columnconfigure(i, minsize=60)
         self.set_buttons()
-        self.curr_player = tk.Label(self, text=f'Current Player:')
+        self.curr_player = tk.Label(self, text=f"Current Player:")
         self.curr_player.grid(row=self.board_size + 1, column=self.board_size + 1)
         exit_button.grid(row=self.board_size + 1, column=0, sticky="nsew")
 
@@ -186,15 +212,18 @@ class PlayPage(tk.Frame, GameView):
                         background = "white smoke"
                 button = 0
                 self.buttons.append(button)
-                self.buttons[i][j] = tk.Button(self, image = None, bg=background,
-                                               command=lambda i=i, j=j:
-                                               self.request_move(i, j, None, None))
-                self.buttons[i][j].grid(row=j, column=i, sticky='nsew')
+                self.buttons[i][j] = tk.Button(
+                    self,
+                    image=None,
+                    bg=background,
+                    command=lambda i=i, j=j: self.request_move(i, j, None, None),
+                )
+                self.buttons[i][j].grid(row=j, column=i, sticky="nsew")
 
 
 if __name__ == "__main__":
     game = Game()
-    controller = Controller(game)
+    # controller = Controller(game)
     game_view = GUIView(controller, game.board)
     controller.set_view(game_view.frames["PlayPage"])
 
